611349302ac6c0aecbb1eac5d8ac2fd1af6de2e8
Joe De Coppi <joe@jdecoppi.com>
2025-08-07 18:55:39 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+
+import './App.css'
+
+function App() {
+
+  /* phases: before quiz has started, quiz ongoing, quiz finished */
+  /* variables per phase:
+  ........before: none
+  ........during: currentQuestion (int), optionA (str), optionB (str), optionC (str), optionD (str), correctAnswers (array int), selectedAnswers (arrayint), output
+  ........after:  */
+
+  /* processing 
+  .......before: just the submit btn to start
+  .......during: connection query, 
+  .......scoreScreen: 
+  */
+  
+  return (
+    
+    
+    <>
+<div className="">
+  <h1>Security+ Quiz - Data</h1>
+  <p>This quiz will present you 10 questions out of aran</p>
+  <button></button>
+</div>
+    
+      
+    </>
+  )
+}
+
+export default App
