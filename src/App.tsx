f1ad6316b4d521eef2bd175f1f91ee6a34b4b108
Joe De Coppi <joe@jdecoppi.com>
2025-08-07 19:29:35 -0400

    declare variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-
+  
+  //scratch notes
   /* phases: before quiz has started, quiz ongoing, quiz finished */
   /* variables per phase:
   ........before: none
@@ -14,18 +16,28 @@ function App() {
   .......during: connection query, 
   .......scoreScreen: 
   */
-  
+
+
+  const [currentQuestionCount, setCurrentQuestionCount] = useState(0);
+  const [currentQuestionText, setCurrentQuestionText] = useState("");
+  const [currentQuestionOptionAText, setCurrentQuestionOptionAText] = useState("");
+  const [currentQuestionOptionBText, setCurrentQuestionOptionBText] = useState("");
+  const [currentQuestionOptionCText, setCurrentQuestionOptionCText] = useState("");
+  const [currentQuestionOptionDText, setCurrentQuestionOptionDText] = useState("");
+  const [currentQuestionCorrectAnswer, setQuestionCorrectAnswer] = useState(0);
+  const [finalOutputString, setFinalOutputString] = useState(0); //not a fan of this but w.e
+
   return (
-    
-    
+
+
     <>
-<div className="">
-  <h1>Security+ Quiz - Data</h1>
-  <p>This quiz will present you 10 questions out of aran</p>
-  <button></button>
-</div>
-    
-      
+      <div className="">
+        <h1>Security+ Quiz - Data</h1>
+        <p>This quiz will present you 10 questions out of a pool of 25, by connecting through a mySQL databsee that is authenticated with Azure Key Vault.</p>
+        <button>Start Quiz</button>
+      </div>
+
+
     </>
   )
 }
